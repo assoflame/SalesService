3f029e90092aec75317b80ae86e95d39dd2f0ccd
arboktya <howaboutno129@gmail.com>
2023-11-04 23:31:14 +0300

    убрал у пользователя поле аватар

---
+++ src/DataAccess/Migrations/20230913111152_remove avatar from user model.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeavatarfromusermodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Avatar",
+                table: "Users",
+                type: "bytea",
+                nullable: true);
+        }
+    }
+}
