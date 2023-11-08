e12396253a705aa71dab4e99385c219d5baeb620
arboktya <howaboutno129@gmail.com>
2023-11-08 21:30:43 +0300

    рефакторинг

--- src/client/src/components/SignInForm.jsx
+++ src/client/src/components/SignInForm.jsx
@@ -3,17 +3,19 @@ import { signIn } from "../helpers/auth";
 
 
 const SignInForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [form, setForm] = useState({
+        email : '',
+        password : ''
+    });
 
     return (
         <form onSubmit={async (e) => {
             e.preventDefault();
-            await signIn({email, password});
+            await signIn(form);
         }}>
             <div className="authInputs">
-                <input name="Email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
-                <input name="Password" type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
+                <input name="Email" placeholder='Email' onChange={(e) => setForm({...form, email : e.target.value})}/>
+                <input name="Password" type="password" placeholder='Password' onChange={(e) => setForm({...form, password : e.target.value})}/>
             </div>
             <div className="authButton">
                 <button type="submit">Sign In</button>
--- src/client/src/components/SignUpForm.jsx
+++ src/client/src/components/SignUpForm.jsx
@@ -2,32 +2,27 @@ import React, { useState } from "react";
 import { signUp } from "../helpers/auth";
 
 const SignUpForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [city, setCity] = useState('');
-    const [age, setAge] = useState('');
+    const [form, setForm] = useState({
+        email : '',
+        password : '',
+        firstName : '',
+        lastName : '',
+        city : '',
+        age : ''
+    });
 
     return (
         <form onSubmit={async (e) => {
             e.preventDefault();
-            await signUp({
-                email,
-                password,
-                firstName,
-                lastName,
-                city,
-                age
-            });
+            await signUp(form);
         }}>
             <div className="authInputs">
-                <input name="Email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
-                <input name="Password" type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
-                <input name="FirstName" placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
-                <input name="LastName" placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
-                <input name="City" placeholder='City' onChange={(e) => setCity(e.target.value)}/>
-                <input name="Age" placeholder='Age' onChange={(e) => setAge(e.target.value)}/>
+                <input name="Email" placeholder='Email' onChange={(e) => setForm({...form, email : e.target.value})}/>
+                <input name="Password" type="password" placeholder='Password' onChange={(e) => setForm({...form, password : e.target.value})}/>
+                <input name="FirstName" placeholder='First Name' onChange={(e) => setForm({...form, firstName : e.target.value})}/>
+                <input name="LastName" placeholder='Last Name' onChange={(e) => setForm({...form, lastName : e.target.value})}/>
+                <input name="City" placeholder='City' onChange={(e) => setForm({...form, city : e.target.value})}/>
+                <input name="Age" placeholder='Age' onChange={(e) => setForm({...form, age : e.target.value})}/>
             </div>
             <div className="authButton">
                 <button type="submit">Sign Up</button>
